d34298dfdec40e2ed4f39ecf285c6db539bf9944
Ed <65623961+efong505@users.noreply.github.com>
2022-08-06 20:18:38 -0600

    Update settings.py

--- julias2/settings.py
+++ julias2/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -154,6 +154,6 @@ MEDIA_URL = '/images/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
