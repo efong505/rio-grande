ed794d09565e8d6ef2a50e63d4421931d180fbd2
Ed <65623961+efong505@users.noreply.github.com>
2022-08-07 21:37:53 -0600

    Update models.py

--- base/models.py
+++ base/models.py
@@ -1,7 +1,7 @@
 from distutils.text_file import TextFile
 from statistics import mode
 from unicodedata import category
-import uuid
+#import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
