13762a40eb81e46f1aaeb01f0d645b0b8606ba1f
Ed <65623961+efong505@users.noreply.github.com>
2022-08-08 09:35:54 -0600

    Update prod_settings.py

--- julias2/prod_settings.py
+++ julias2/prod_settings.py
@@ -8,7 +8,7 @@ import django_heroku
 DEBUG = False
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 # adjust to the URL of your Heroku app
-ALLOWED_HOSTS = ['rio-grande.herokuapp.com']
+ALLOWED_HOSTS = ['rio-grande-art.herokuapp.com']
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
