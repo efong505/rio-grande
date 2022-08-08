46c65047740620439e52d17ea49a9295b9bdcd67
Ed <65623961+efong505@users.noreply.github.com>
2022-08-07 21:34:23 -0600

    Update models.py

--- base/models.py
+++ base/models.py
@@ -103,8 +103,8 @@ class About(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.CharField(default=uuid.uuid4, 
-                          primary_key=False, editable=False, max_length=36)
+    #id = models.CharField(default=uuid.uuid4, 
+     #                     primary_key=False, editable=False, max_length=36)
     #id = models.UUIDField(primary_key=True,default=uuid.uuid4, unique=True)
                           
 
