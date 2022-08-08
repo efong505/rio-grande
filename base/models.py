49f3144aaca969d04d2b368f2a930dfafe0f4ccb
Ed <65623961+efong505@users.noreply.github.com>
2022-08-08 06:27:14 -0600

    Update models.py

--- base/models.py
+++ base/models.py
@@ -42,7 +42,7 @@ class Post(models.Model):
     #thumbnail = models.ImageField()
     # categories = models.ManyToManyField(Category, null=True)
     featured = models.BooleanField()
-    id=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
+    
     class Meta:
         ordering = ['-created_on']
     def __str__(self):
@@ -57,7 +57,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
-    id=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
+   
     class Meta:
         ordering = ['created_on']
     def __str__(self):
@@ -92,7 +92,7 @@ class About(models.Model):
     story = models.TextField()
     image = models.ImageField(null=True, blank=True,
                                        default="default.jpg")
-    id=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
+    UUID = models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
     class Meta:
         verbose_name_plural = "About Options"
     def __str__(self):
