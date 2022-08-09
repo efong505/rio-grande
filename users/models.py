f0e756fdf5aae5ee1eaa901b1220cc6cf37f7a6d
Ed <65623961+efong505@users.noreply.github.com>
2022-08-08 19:20:19 -0600

    Update models.py

--- users/models.py
+++ users/models.py
@@ -17,7 +17,8 @@ class Profile(models.Model):
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
     created = models.DateTimeField(auto_now_add=True)
-    #id=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     
     def __str__(self):
         return str(self.username)
